509b74200848471a8fc9ed91930c21b54d1b2e82
Will Jones <will-anthony@hotmail.com>
2023-01-27 07:05:55 +0000

    Initial commit
    Includes:
    * Adding 3 fragments and navbar which allows you to move between the 3. Tested and working.
    * Currently gitignore is based on Jetbrains recommendation. Most of .idea folder committed. May have to edit in the future.

---
+++ app/src/main/java/uk/co/willanthony/myrecipes/MainActivity.kt
@@ -0,0 +1,72 @@
+package uk.co.willanthony.myrecipes
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.navHostFragment)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.recipeFragment,
+                R.id.favouriteRecipeFragment,
+                R.id.inventionsFragment
+            )
+        )
+
+        bottomNavigationView.setupWithNavController(navController)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
